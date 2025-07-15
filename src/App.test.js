790d3d5925e7ef8171817c6f53e2d71b45d8a76b
Nouman <syednoumanquadrii@gamil.com>
2025-07-15 18:06:21 +0530

    hehe

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://seoop.onrender.com';
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
